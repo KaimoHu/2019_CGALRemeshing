7ecaf4c91117364ee42cbe7c5ed026a08d23139c
Kaimo Hu <hukaimo02@gmail.com>
2019-07-21 10:14:05 +0800

    07212019: built up the CGAL_remeshing project and the min angle remeshing algorithm.

---
+++ src/Viewer.h
@@ -0,0 +1,35 @@
+#ifndef CGAL_VIEWER_H
+#define CGAL_VIEWER_H
+
+#include <QMap>
+#include <CGAL/Qt/qglviewer.h>
+
+// forward declarations
+class QWidget;
+class Scene;
+class Viewer : public CGAL::QGLViewer {
+
+  Q_OBJECT
+
+ public:
+  explicit Viewer(QWidget *parent);
+  Viewer(const Viewer &) = delete;
+  Viewer &operator = (const Viewer &) = delete;
+  Viewer(const Viewer &&) = delete;
+  Viewer &operator = (const Viewer &&) = delete;
+
+  // overload several QGLViewer virtual functions
+  void draw();
+  void initializeGL();
+  void setScene(Scene *pScene);
+
+ protected:
+  /*virtual void mousePressEvent(QMouseEvent *e);
+  virtual void mouseReleaseEvent(QMouseEvent *e);*/
+
+ private:
+  Scene *m_pScene;
+  bool m_custom_mouse;
+}; // end class Viewer
+
+#endif // CGAL_VIEWER_H
